04ca532d15f196497efcd3be5163a2acf3005361
utsavvakil <utsavvakil@gmail.com>
2018-02-08 13:49:05 -0500

    Added Peephole code

--- pictureServer.js
+++ pictureServer.js
@@ -88,6 +88,17 @@ serial.pipe(parser);
 parser.on('data', function(data) {
   console.log('Data:', data);
   io.emit('server-msg', data);
+  if (data === 'light') {
+   var imageName = new Date().toString().replace(/[&\/\\#,+()$~%.'":*?<>{}\s-]/g, '');
+
+    console.log('making a making a picture at'+ imageName); // Second, the name is logged to the console.
+
+    //Third, the picture is  taken and saved to the `public/`` folder
+    NodeWebcam.capture('public/'+imageName, opts, function( err, data ) {
+    io.emit('newPicture',(imageName+'.jpg')); ///Lastly, the new name is send to the client web browser.
+    /// The browser will take this new name and load the picture from the public folder.
+   });
+  }
 });
 //----------------------------------------------------------------------------//
 
